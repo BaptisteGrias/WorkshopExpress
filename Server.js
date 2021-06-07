a23f8fc13e74f0fd6e41d244db85903cced351fe
Baptiste Grias <baptistegrias@MBP-de-Baptiste.home>
2021-06-07 19:09:08 +0200

    ok

--- Server.js
+++ Server.js
@@ -4,66 +4,8 @@ const connection = require('./Config');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-const homeRouter = require('./routes/home');
-const productsRouter = require('./routes/products');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use('/home', homeRouter);
-app.use('/products', productsRouter);
-
-// app.get('/products', (req, res) => {
-//   connection.query('SELECT * FROM products', (err, results) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error retrieving products from db.');
-//     } else {
-//       res.json(results);
-//     }
-//   });
-// });
-
-app.get('/products', (req, res) => {
-  connection
-    .promise()
-    .query('SELECT * FROM products')
-    .then(([results]) => {
-      res.json(results);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error retrieving products from db.');
-    });
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  connection
-    .promise()
-    .query('SELECT * FROM products WHERE id = ?', [id])
-    .then(([results]) => {
-      if (results.length) {
-        res.json(results[0]);
-      } else {
-        res.sendStatus(404);
-      }
-    });
-});
-
-app.delete('/products/:id', (req, res) => {
-  connection
-    .promise()
-    .query('DELETE FROM products WHERE id = ?', [req.params.id])
-    .then(([result]) => {
-      if (result.affectedRows) res.sendStatus(204);
-      else res.sendStatus(404);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
 
 connection.connect((err) => {
   if (err) {
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const connection = require('../Config');
 const router = express.Router();
+const homeRouter = require('./routes/home');
+const productsRouter = require('./routes/products');
+
+router.use('/home', homeRouter);
+router.use('/products', productsRouter);
+
+router.get('/products', (req, res) => {
+  connection
+    .promise()
+    .query('SELECT * FROM products')
+    .then(([results]) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error retrieving products from db.');
+    });
+});
+
+router.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  connection
+    .promise()
+    .query('SELECT * FROM products WHERE id = ?', [id])
+    .then(([results]) => {
+      if (results.length) {
+        res.json(results[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    });
+});
 
 router.post('/', (req, res) => {
   const { name, price } = req.body;
@@ -13,6 +45,33 @@ router.post('/', (req, res) => {
       res.status(200).json(result);
     }
   });
+});
+
+router.put('./id', (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const sql = `UPDATE products SET (?, ?) WHERE id=? `;
+  connection.query(sql, [name, price, id], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'error query database' });
+    } else {
+      res.status(200).json({ result: 'modification ok' });
+    }
+  });
+});
+
+router.delete('/products/:id', (req, res) => {
+  connection
+    .promise()
+    .query('DELETE FROM products WHERE id = ?', [req.params.id])
+    .then(([result]) => {
+      if (result.affectedRows) res.sendStatus(204);
+      else res.sendStatus(404);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
